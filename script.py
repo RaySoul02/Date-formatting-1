b946f119ca88d8bdf409d5af467850f90b0adbc7
RaySoul02 <102035925+RaySoul02@users.noreply.github.com>
2022-04-24 12:13:17 +0600

    Delete script.py

--- script.py
+++
@@ -1,7 +0,0 @@
-import csv
-r = csv.reader(open('C:\\Users\\nurzh\\Desktop\\source.csv'))
-lines = list(r)
-lines[1][0] = '2020-1-1'
-lines[2][0] = '2021-1-1'
-writer = csv.writer(open('C:\\Users\\nurzh\\Desktop\\result.csv', 'w'))
-writer.writerows(lines)
